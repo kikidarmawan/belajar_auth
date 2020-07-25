224158a8f3cf0c1f83b423d0d3e1744aae4c8372
Kiki Darmawan <darmawankiki02@gmail.com>
2020-07-25 17:02:04 +0700

    url redirect login

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,12 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/dashboard', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
